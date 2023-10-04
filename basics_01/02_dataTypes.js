47c79b550b1f7a721d7a130fda66884f75a90736
harry <sachana369@gmail.com>
2023-10-04 21:23:04 +0530

    data type

---
+++ basics_01/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all js code as newer version
+
+// alert (3+3) // we are using in nodeJS ,not in browser
+
+console.log (3+3)
+console.log("abhishek")
+
+let name = "abhishek"
+let age = 23
+let isLoggedIN = false
+
+//number => 2  to power 53
+//string => " "
+//boolean => true, false
+//null => standalone value
+//undefined => not defined value
+//symbol => unique
+
+
+//object
+
+
+console.log(typeof null) //object
