687a5e4b206fe7cd1c8fe04836bf8ef0e1eafdde
harry <sachana369@gmail.com>
2023-10-04 21:05:22 +0530

    change

---
+++ basics_01/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 12345
+let accountEmail = "abhisheksachan529@gmail.com"
+var accountPassword = "132345"
+accountCity = "lucknow"
+
+
+
+// accountId = 123
+// Assignment to constant variable.
+
+accountEmail = "sachan@gmail.com"
+accountPassword = "7894"
+accountCity = "kanpur"
+
+// console.log(accountId)
+console.table([accountId, accountEmail, accountPassword, accountCity])
